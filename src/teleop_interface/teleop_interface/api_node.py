8e112ba72e57933b4ecbbb31badb4770a3d9adc1
rajitzg <rzghosh@gmail.com>
2026-03-06 09:50:22 -0800

    revert allow credentials to true

--- src/teleop_interface/teleop_interface/api_node.py
+++ src/teleop_interface/teleop_interface/api_node.py
@@ -14,7 +14,7 @@ app = FastAPI(title="Robot Teleop API", description="REST API for Robot Controls
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=False,
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
